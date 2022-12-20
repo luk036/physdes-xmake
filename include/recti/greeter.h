1f0a1d039db8b980e40f54010ef2c7a2a1e35f02
Wai-Shing Luk <luk036@gmail.com>
2022-12-20 03:19:29 +0000

    apply trunk

--- include/recti/greeter.h
+++ include/recti/greeter.h
@@ -4,28 +4,28 @@
 
 namespace recti {
 
-    /**  Language codes to be used with the Recti class */
-    enum class LanguageCode { EN, DE, ES, FR };
+/**  Language codes to be used with the Recti class */
+enum class LanguageCode { EN, DE, ES, FR };
 
-    /**
-     * @brief A class for saying hello in multiple languages
-     */
-    class Recti {
-        std::string name;
+/**
+ * @brief A class for saying hello in multiple languages
+ */
+class Recti {
+  std::string name;
 
-      public:
-        /**
-         * @brief Creates a new recti
-         * @param[in] name the name to greet
-         */
-        Recti(std::string name);
+public:
+  /**
+   * @brief Creates a new recti
+   * @param[in] name the name to greet
+   */
+  Recti(std::string name);
 
-        /**
-         * @brief Creates a localized string containing the greeting
-         * @param[in] lang the language to greet in
-         * @return a string containing the greeting
-         */
-        std::string greet(LanguageCode lang = LanguageCode::EN) const;
-    };
+  /**
+   * @brief Creates a localized string containing the greeting
+   * @param[in] lang the language to greet in
+   * @return a string containing the greeting
+   */
+  std::string greet(LanguageCode lang = LanguageCode::EN) const;
+};
 
-}  // namespace recti
+} // namespace recti
